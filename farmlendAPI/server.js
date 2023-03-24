b0f3213d7315e64047236cbff69c1c55faa02faf
Zenith Code Services <ZenithCodeServices@Tims-MBP-2.broadband>
2023-03-24 17:40:52 +0000

    Make credentials environment variables

--- farmlendAPI/server.js
+++ farmlendAPI/server.js
@@ -3,11 +3,11 @@ const PORT = process.env.PORT || 3000;
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'P9Amlverb',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 const app = express();
